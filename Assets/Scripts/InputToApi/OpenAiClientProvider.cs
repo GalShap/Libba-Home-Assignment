19ed17f7e0b5457404f99f3bfedf2fb702ee174a
GalShaffir <80719879+GalShap@users.noreply.github.com>
2023-12-02 19:04:43 +0200

    made api request process more generic.
    Instead of a function for every input type there is now one function for all.

---
+++ Assets/Scripts/InputToApi/OpenAiClientProvider.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using OpenAI;
+using OpenAI.Chat;
+using OpenAI.Models;
+using UnityEngine;
+using Utilities;
+
+namespace InputToApi
+{
+    public class OpenAiClientProvider : SingletonPersistent<OpenAiClientProvider>
+    {
+        [SerializeField] private string pathToDirectory;
+        
+        private OpenAIClient _openAIClient;
+
+        private void Start()
+        {
+            _openAIClient = new OpenAIClient(new OpenAIAuthentication().
+                LoadFromDirectory(pathToDirectory));
+        }
+
+        public async Task<ChatResponse> SendContent(List<Content> contents, string model)
+        {
+            var messages = new List<Message>
+            {
+                new(Role.System, "You are a helpful assistant."),
+                new(Role.User, contents)
+            };
+            
+            var chatRequest = new ChatRequest(messages, model);
+            var result = await _openAIClient.ChatEndpoint.GetCompletionAsync(chatRequest);
+            return result;
+            
+        }
+    }
+}
--- Assets/Scripts/Utilities/Constants.cs
+++ Assets/Scripts/Utilities/Constants.cs
@@ -1,9 +1,13 @@
-﻿namespace Utilities
+﻿using OpenAI.Chat;
+
+namespace Utilities
 {
     public static class Constants
     {
 
         public const int Empty = 0;
+
+        public const string Vision = "gpt-4-vision-preview";
 
 
     }
